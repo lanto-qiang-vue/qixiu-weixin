b35d90be68a969d908f6b36cfca61e30c26164ac
lanto <lanto@lantodeMacBook.local>
2019-03-22 14:30:14 +0800

    3.22

--- src/router.js
+++ src/router.js
@@ -139,7 +139,7 @@ export default new Router({
 		{path: '/myAppointment', name: 'myAppointment', component: resolve => require(['@/page/myAppointment'], resolve), meta: {requiresAuth: true, index: 20}},
 		{path: '/myOrder', name: 'myOrder', component: resolve => require(['@/page/myOrder'], resolve), meta: {requiresAuth: true, index: 20}},
 		{path: '/myRemark', name: 'myRemark', component: resolve => require(['@/page/remark/myRemark'], resolve), meta: {requiresAuth: true}},
-		{path: '/remarkMap', name: 'remark-map', component: resolve => require(['@/page/service-map/maintain'], resolve), meta: {keepAlive: false}},
+		{path: '/remarkMap', name: 'remark-map', component: resolve => require(['@/page/service-map/maintain'], resolve), meta: {keepAlive: false,  requiresAuth: true}},
 		{path: '/remarkMatch', name: 'remark-match', component: resolve => require(['@/page/remark/match'], resolve),meta: {requiresAuth: true}},
 		{path: '/remarkDetail', name: 'remark-detail', component: resolve => require(['@/page/remark/newRemarkDetail'], resolve)},
 		{path: '/maintainRemark', name: 'maintainRemark', component: resolve => require(['@/page/maintainRemark'], resolve)},
