04d672c25da43ac17c7d7fc362106b376b3f16ef
lanto <lanto@lantodeMacBook.local>
2019-07-19 15:07:26 +0800

    7.19

--- src/page/com-remind.vue
+++ src/page/com-remind.vue
@@ -5,7 +5,7 @@
 	<div class="content" v-show="show">根据《机动车维修管理规定》(交通运输部令2016年第37号) 第三十四条要求，机动车维修经营者应当按照规定如实填报、及时上传承修机动车的维修电子数据记录至国家有关汽车电子健康档案系统。
 	</div>
 	<div class="content" v-show="show">未按规定上传维修记录的维修企业，将被列入维修企业黑名单并对外公示。</div>
-	<div class="content" v-show="!show">您可登录“上海汽修平台”（www.shanghaiqixiu.org）– “管理中心” – “日常监管” – “上传监控”，点击柱状图上对应颜色区域查看企业详情。</div>
+	<div class="content" v-show="!show">您可登录“上海汽修平台”（www.shanghaiqixiu.org）– “管理中心” – “日常监管” – “上传监控”，点击柱状图上对应颜色区域查看详情。</div>
 </div>
 </template>
 
